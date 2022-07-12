110bcca8319fdb9167f97cdd67098c85d46f1ab1
Mohammed Said Al Busaidi <70335350+cyperOM@users.noreply.github.com>
2022-07-12 17:13:36 +0400

    Update passPDF.py

--- passPDF.py
+++ passPDF.py
@@ -1,3 +1,5 @@
+#code by cyperom
+#This python code will allow you to protect your PDF's with password with a click of a button. 
 import PyPDF2
 import os
 
@@ -10,7 +12,6 @@ for file in os.listdir():
 print(files)
 for file in os.listdir():
 
-#pdf_in_file = open("simple.pdf",'rb')
     pdf_in_file = open(file,'rb')
 
     inputpdf = PyPDF2.PdfFileReader(pdf_in_file)
@@ -21,10 +22,9 @@ for file in os.listdir():
         inputpdf = PyPDF2.PdfFileReader(pdf_in_file)
     
     output.addPage(inputpdf.getPage(i))
-    output.encrypt('cyperom')
+    output.encrypt('cyperom') #you can change the password (cyperom) to any password you want 
 
-    #with open("simple_password_protected.pdf", "wb") as outputStream:
-    with open("password"+file, "wb") as outputStream:
+    with open("password"+file, "wb") as outputStream:  #you can rename your file by replacing the ("password"+file) to anything you want but if you are protecting multible files make sure you replace it with something dynamic 
         output.write(outputStream)
 
     pdf_in_file.close()
